9daa24c402dcafe7aeba3d00991445a03e3ac5b4
Kelvin Mathew <kelvinmathew2004@gmail.com>
2025-06-16 12:25:20 -0500

    Added riddles dictionary

---
+++ src/Riddles.jsx
@@ -0,0 +1,52 @@
+export const Riddles = [
+  {
+    question: "What has keys but can’t open locks?",
+    answer: "A piano.",
+    difficulty: "easy",
+  },
+  {
+    question: "What gets wetter the more it dries?",
+    answer: "A towel.",
+    difficulty: "easy",
+  },
+  {
+    question: "What has hands but can’t clap?",
+    answer: "A clock.",
+    difficulty: "easy",
+  },
+  {
+    question: "The more you take away from me, the bigger I get. What am I?",
+    answer: "A hole.",
+    difficulty: "medium",
+  },
+  {
+    question: "What begins with T, ends with T, and has T in it?",
+    answer: "A teapot.",
+    difficulty: "medium",
+  },
+  {
+    question: "What belongs to you but is used more by others?",
+    answer: "Your name.",
+    difficulty: "medium",
+  },
+  {
+    question: "I’m tall when I’m young and short when I’m old. What am I?",
+    answer: "A candle.",
+    difficulty: "medium",
+  },
+  {
+    question: "What can travel around the world while staying in the same corner?",
+    answer: "A stamp.",
+    difficulty: "hard",
+  },
+  {
+    question: "What has one eye but can’t see?",
+    answer: "A needle.",
+    difficulty: "hard",
+  },
+  {
+    question: "What comes down but never goes up?",
+    answer: "Rain.",
+    difficulty: "hard",
+  },
+];
